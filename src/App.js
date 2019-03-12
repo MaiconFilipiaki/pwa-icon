23da9f99922d59cc36fe7abe2b3b2a9644fbda31
MaiconFilipiaki <mdfilipiaki@gmail.com>
2019-03-12 08:04:29 -0300

    Link live

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
 
+import { Header, Repositories, Offline } from "./styles";
 class App extends Component {
+  state = {
+    newRepoInput: "",
+    online: navigator.onLine,
+    repositories: JSON.parse(localStorage.getItem("repositories")) || []
+  };
+
+  componentDidMount() {
+    window.addEventListener("online", this.handlerNetworkChange);
+    window.addEventListener("offline", this.handlerNetworkChange);
+  }
+
+  componentWillMount() {
+    window.removeEventListener("online", this.handlerNetworkChange);
+    window.removeEventListener("offline", this.handlerNetworkChange);
+  }
+
+  handlerNetworkChange = () => {
+    this.setState({ online: navigator.onLine });
+  };
+
+  addRepository = async () => {
+    if (!this.state.newRepoInput) return;
+
+    if (!this.state.online) alert("Voce esta offline");
+
+    const response = await axios.get(
+      `https://api.github.com/repos/${this.state.newRepoInput}`
+    );
+
+    this.setState({
+      newRepoInput: "",
+      repositories: [...this.state.repositories, response.data]
+    });
+    localStorage.setItem(
+      "repositories",
+      JSON.stringify(this.state.repositories)
+    );
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Header>
+          <input
+            placeholder="Add repositorio"
+            value={this.state.newRepoInput}
+            onChange={e => this.setState({ newRepoInput: e.target.value })}
+          />
+          <button onClick={this.addRepository}>Add</button>
+        </Header>
+
+        <Repositories>
+          {this.state.repositories.map(repository => (
+            <li key={repository.id}>
+              <img src={repository.owner.avatar_url} />
+              <div>
+                <strong>{repository.name}</strong>
+                <p>{repository.description}</p>
+                <a href={repository.html_url}>Acessar</a>
+              </div>
+            </li>
+          ))}
+        </Repositories>
+        {!this.state.online && <Offline>Voce esta Offline</Offline>}
+      </Fragment>
     );
   }
 }
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 102%;
+  background: rebeccapurple;
+  height: 3rem;
+  margin-top: -8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: -8px;
+  padding-top: 15px;
+`;
+
+export const Repositories = styled.ul``;
+
+export const Offline = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 40px;
+  width: 100%;
+  background: #c15959;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 600;
+`;
